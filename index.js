50e39f1e55ec1623dcdb5f3612b810fef0716152
Kenneth Portillo <portillokenneth@gmail.com>
2019-05-14 04:00:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  var theBeatles = []
+  
+  for (let i = 0; i < musicians.length; i++){
+    theBeatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return theBeatles
+}
+function johnLennonFacts(facts){
+ var john = []
+  let i = 0
+  while (i < facts.length){
+   john.push(`${facts[i]}!!!`)
+  }
+  i++
+  return john
+}
